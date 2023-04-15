4d7a37d572b462f8431471e479524bcd445777a9
king-gheedorah <brandon.cooper123@gmail.com>
2023-04-14 23:51:41 -0700

    Add commas to keymaps

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -49,31 +49,21 @@ XXX  XXX  XXX  XXX  XXX                                                         
 &kp GRAVE  K20  K21  K22  K23  K24  &kp LSHFT  &kp LCTRL  &kp LALT  &kp RALT  &kp RCTRL  &kp RSHFT  K25  K26  K27  K28  K29  XXX \
 XXX  XXX  XXX  XXX  XXX   K33  K32  K34  K35  K37  K36   XXX  XXX  XXX  XXX  XXX
 
-#define MIRYOKU_LAYER_GAME(\
-K00, K01, K02, K03, K04,                                    K05, K06, K07, K08, K09, \
-K10, K11, K12, K13, K14,                                    K15, K16, K17, K18, K19, \
-K20, K21, K22, K23, K24,                                    K25, K26, K27, K28, K29, \
-N30, N31, K32, K33, K34,                                    K35, K36, K37, N38, N39 \
-) \
-&kp F1   &kp F2   &kp F3   &kp F4   &kp F5                                           &kp F6   &kp F7   &kp F8   &kp F9   &kp F10 \
-&kp KP_N1  &kp KP_N2  &kp KP_N3  &kp KP_N4  &kp KP_N5                                &kp KP_N6  &kp KP_N7  &kp KP_N8  &kp KP_N9  &kp KP_N0 \
-&kp TAB  &kp TAB  K00  K01  K02  K03  K04                                     K05  K06  K07  K08  K09  \
-&kp ESC  &kp LSHFT  K10  K11  K12  K13  K14                                     K15  K16  K17  K18  K19 \
-&kp GRAVE  &kp LCTRL  K20  K21  K22  K23  K24  &kp LSHFT  &kp LCTRL  &kp LALT       &kp RALT  &kp RCTRL  &kp RSHFT K25  K26  K27  K28  K29  \
-&magic  XXX  XXX  XXX  XXX       K33  K32  K34       K35  K37  K36       XXX  XXX  XXX  XXX  XXX
+#define MIRYOKU_LAYER_GAME \
+&kp F1,   &kp F2,   &kp F3,   &kp F4,   &kp F5,                                           &kp F6,   &kp F7,   &kp F8,   &kp F9,   &kp F10, \
+&kp KP_N1,  &kp KP_N2,  &kp KP_N3,  &kp KP_N4,  &kp KP_N5,                                &kp KP_N6,  &kp KP_N7,  &kp KP_N8,  &kp KP_N9,  &kp KP_N0, \
+&kp TAB,  &kp TAB,  K00,  K01,  K02,  K03,  K04,                                     K05,  K06,  K07,  K08,  K09,  \
+&kp ESC,  &kp LSHFT,  K10,  K11,  K12,  K13,  K14,                                     K15,  K16,  K17,  K18,  K19, \
+&kp GRAVE,  &kp LCTRL,  K20,  K21,  K22,  K23,  K24,  &kp LSHFT,  &kp LCTRL,  &kp LALT,       &kp RALT,  &kp RCTRL,  &kp RSHFT, K25,  K26,  K27,  K28,  K29,  \
+&magic,  XXX,  XXX,  XXX,  XXX,       K33,  K32,  K34,       K35,  K37,  K36,       XXX,  XXX,  XXX,  XXX,  XXX
 
-#define MIRYOKU_LAYER_MAGIC(\
-K00, K01, K02, K03, K04,                                    K05, K06, K07, K08, K09, \
-K10, K11, K12, K13, K14,                                    K15, K16, K17, K18, K19, \
-K20, K21, K22, K23, K24,                                    K25, K26, K27, K28, K29, \
-N30, N31, K32, K33, K34,                                    K35, K36, K37, N38, N39 \
-) \
-&bt BT_CLR            &none            &none            &none            &none                                                                                  &none   &none   &none   &none  &bt BT_CLR_ALL \
-&none            &to U_BASE      &to U_GAME     &none            &none                                                         &none   &none   &none   &none   &none           &none \
-&trans  &rgb_ug RGB_SPI  &rgb_ug RGB_SAI  &rgb_ug RGB_HUI  &rgb_ug RGB_BRI  &rgb_ug RGB_TOG                                                         &none   &none   &none   &none   &none           &none \
-&bootloader  &rgb_ug RGB_SPD  &rgb_ug RGB_SAD  &rgb_ug RGB_HUD  &rgb_ug RGB_BRD  &rgb_ug RGB_EFF                                                         &none   &none   &none   &none   &none     &bootloader \
-&reset            &none            &none            &none            &none            &none   &bt_2   &bt_3         &none   &none   &none   &none   &none   &none   &none   &none   &none          &reset \
-&none            &none            &none            &none            &none                    &bt_0   &bt_1  &out OUT_USB   &none   &none   &none           &none   &none   &none   &none           &none
+#define MIRYOKU_LAYER_MAGIC \
+&bt BT_CLR,            &none,            &none,            &none,            &none,                                                                                  &none,   &none,   &none,   &none,  &bt BT_CLR_ALL, \
+&none,            &to U_BASE,      &to U_GAME,     &none,            &none,                                                         &none,   &none,   &none,   &none,   &none,           &none, \
+&trans,  &rgb_ug RGB_SPI,  &rgb_ug RGB_SAI,  &rgb_ug RGB_HUI,  &rgb_ug RGB_BRI,  &rgb_ug RGB_TOG,                                                         &none,   &none,   &none,   &none,   &none,           &none, \
+&bootloader,  &rgb_ug RGB_SPD,  &rgb_ug RGB_SAD,  &rgb_ug RGB_HUD,  &rgb_ug RGB_BRD,  &rgb_ug RGB_EFF,                                                         &none,   &none,   &none,   &none,   &none,     &bootloader, \
+&reset,            &none,            &none,            &none,            &none,            &none,   &bt_2,   &bt_3,         &none,   &none,   &none,   &none,   &none,   &none,   &none,   &none,   &none,          &reset, \
+&none,            &none,            &none,            &none,            &none,                    &bt_0,   &bt_1,  &out OUT_USB,   &none,   &none,   &none,           &none,   &none,   &none,   &none,           &none
 
 #define MIRYOKU_LAYERMAPPING_GAME MIRYOKU_MAPPING
 #define MIRYOKU_LAYERMAPPING_MAGIC MIRYOKU_MAPPING
