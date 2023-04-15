04e77362759d99995eee686bda324e2ef5b6fa5d
king-gheedorah <brandon.cooper123@gmail.com>
2023-04-14 22:51:10 -0700

    Testing changes to fix build

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -42,11 +42,11 @@ MIRYOKU_X(MAGIC,  "Magic")
 
 #define XXX &none
 
-#define MIRYOKU_LAYOUTMAPPING_GLOVE80( \
-     K00, K01, K02, K03, K04,                                    K05, K06, K07, K08, K09, \
-     K10, K11, K12, K13, K14,                                    K15, K16, K17, K18, K19, \
-     K20, K21, K22, K23, K24,                                    K25, K26, K27, K28, K29, \
-     N30, N31, K32, K33, K34,                                    K35, K36, K37, N38, N39 \
+#define MIRYOKU_LAYOUTMAPPING_GLOVE80(\
+K00, K01, K02, K03, K04,                                    K05, K06, K07, K08, K09, \
+K10, K11, K12, K13, K14,                                    K15, K16, K17, K18, K19, \
+K20, K21, K22, K23, K24,                                    K25, K26, K27, K28, K29, \
+N30, N31, K32, K33, K34,                                    K35, K36, K37, N38, N39 \
 ) \
 &kp F1   &kp F2   &kp F3   &kp F4   &kp F5                                           &kp F6   &kp F7   &kp F8   &kp F9   &kp F10 \
 &kp KP_N1  &kp KP_N2  &kp KP_N3  &kp KP_N4  &kp KP_N5                                &kp KP_N6  &kp KP_N7  &kp KP_N8  &kp KP_N9  &kp KP_N0 \
@@ -55,11 +55,11 @@ MIRYOKU_X(MAGIC,  "Magic")
 &kp GRAVE K20  K21  K22  K23  K24  &kp LSHFT  &kp LCTRL  &kp LALT       &kp RALT  &kp RCTRL  &kp RSHFT K25  K26  K27  K28  K29  XXX \
 &magic  XXX  XXX  XXX  XXX       K33  K32  K34       K35  K37  K36       XXX  XXX  XXX  XXX  XXX
 
-#define MIRYOKU_LAYERMAPPING_GAME( \
-     K00, K01, K02, K03, K04,                                    K05, K06, K07, K08, K09, \
-     K10, K11, K12, K13, K14,                                    K15, K16, K17, K18, K19, \
-     K20, K21, K22, K23, K24,                                    K25, K26, K27, K28, K29, \
-     N30, N31, K32, K33, K34,                                    K35, K36, K37, N38, N39 \
+#define MIRYOKU_LAYERMAPPING_GAME(\
+K00, K01, K02, K03, K04,                                    K05, K06, K07, K08, K09, \
+K10, K11, K12, K13, K14,                                    K15, K16, K17, K18, K19, \
+K20, K21, K22, K23, K24,                                    K25, K26, K27, K28, K29, \
+N30, N31, K32, K33, K34,                                    K35, K36, K37, N38, N39 \
 ) \
 &kp F1   &kp F2   &kp F3   &kp F4   &kp F5                                           &kp F6   &kp F7   &kp F8   &kp F9   &kp F10 \
 &kp KP_N1  &kp KP_N2  &kp KP_N3  &kp KP_N4  &kp KP_N5                                &kp KP_N6  &kp KP_N7  &kp KP_N8  &kp KP_N9  &kp KP_N0 \
@@ -68,11 +68,11 @@ MIRYOKU_X(MAGIC,  "Magic")
 &kp GRAVE  &kp LCTRL  K20  K21  K22  K23  K24  &kp LSHFT  &kp LCTRL  &kp LALT       &kp RALT  &kp RCTRL  &kp RSHFT K25  K26  K27  K28  K29  \
 &magic  XXX  XXX  XXX  XXX       K33  K32  K34       K35  K37  K36       XXX  XXX  XXX  XXX  XXX
 
-#define MIRYOKU_LAYERMAPPING_MAGIC( \
-     K00, K01, K02, K03, K04,                                    K05, K06, K07, K08, K09, \
-     K10, K11, K12, K13, K14,                                    K15, K16, K17, K18, K19, \
-     K20, K21, K22, K23, K24,                                    K25, K26, K27, K28, K29, \
-     N30, N31, K32, K33, K34,                                    K35, K36, K37, N38, N39 \
+#define MIRYOKU_LAYERMAPPING_MAGIC(\
+K00, K01, K02, K03, K04,                                    K05, K06, K07, K08, K09, \
+K10, K11, K12, K13, K14,                                    K15, K16, K17, K18, K19, \
+K20, K21, K22, K23, K24,                                    K25, K26, K27, K28, K29, \
+N30, N31, K32, K33, K34,                                    K35, K36, K37, N38, N39 \
 ) \
 &bt BT_CLR            &none            &none            &none            &none                                                                                  &none   &none   &none   &none  &bt BT_CLR_ALL \
 &none            &to U_BASE      &to U_GAME     &none            &none                                                         &none   &none   &none   &none   &none           &none \
