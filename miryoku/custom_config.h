8dcb5ef7f703e3a377818717e84be0626df91912
king-gheedorah <brandon.cooper123@gmail.com>
2023-04-14 23:59:05 -0700

    Testing keymap

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -37,10 +37,10 @@ MIRYOKU_X(MAGIC,  "Magic")
 #define XXX &none
 
 #define MIRYOKU_LAYOUTMAPPING_GLOVE80(\
-K00, K01, K02, K03, K04,                                    K05, K06, K07, K08, K09, \
-K10, K11, K12, K13, K14,                                    K15, K16, K17, K18, K19, \
-K20, K21, K22, K23, K24,                                    K25, K26, K27, K28, K29, \
-N30, N31, K32, K33, K34,                                    K35, K36, K37, N38, N39 \
+     K00, K01, K02, K03, K04,                                    K05, K06, K07, K08, K09, \
+     K10, K11, K12, K13, K14,                                    K15, K16, K17, K18, K19, \
+     K20, K21, K22, K23, K24,                                    K25, K26, K27, K28, K29, \
+     N30, N31, K32, K33, K34,                                    K35, K36, K37, N38, N39 \
 ) \
 XXX  XXX  XXX  XXX  XXX                                                              XXX  XXX  XXX  XXX  XXX \
 &kp KP_N1  &kp KP_N2  &kp KP_N3  &kp KP_N4  &kp KP_N5  &kp KP_N6  &kp KP_N7  &kp KP_N8  &kp KP_N9  &kp KP_N0 \
