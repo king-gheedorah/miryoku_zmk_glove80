d80ff02ecc059527fac6873dc182d8f63c46592e
king-gheedorah <brandon.cooper123@gmail.com>
2023-04-14 23:37:26 -0700

    Remove keyboard definition

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,6 +1,5 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
-#define MIRYOKU_KEYBOARD_GLOVE80
 #define MIRYOKU_ALPHAS_QWERTY
 #define MIRYOKU_TAP_QWERTY
 #define MIRYOKU_NAV_INVERTEDT
